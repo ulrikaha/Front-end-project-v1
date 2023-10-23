eda93fc3aa98449d7bbd426c577ee8041c48f974
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2023-10-23 20:57:55 +0200

    added login auth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {},
+
+            async authorize(credentials) {
+                const user = { id: 1 };
+                return user;
+            },
+        }),
+    ],
+    session: {
+        stategy: "jwt", 
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: "/login",
+    },
+}
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,24 +1,66 @@
-import Link from "next/link";
+'use client';
 
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+    try {
+        const res = await signIn('credentials', {
+            email,
+            password,
+            redirect: false,
+        });
+
+        if (res?.error) {
+            setError("Invalid credentials");
+            return;
+        }
+
+        router.replace('dashboard');
+    } catch (error) {
+        console.log(error);
+    }
+        };
+
 
     return <div className="grid place-items-center h-screen">
     <div className="shadow-lg p-5 rounded-lg border-t-12 border-custom-green -mt-4">
 <h1 className="text-xl font-bold my-4">Login</h1>
 
-<form className="flex flex-col gap-3">
-    <input className="w-[400px] border border-gray-200 font-baskerville" type="text" placeholder="Email" />
-    <input className="w-[400px] border border-gray-200 font-baskerville" type="password" placeholder="Password" />
+<form onSubmit={handleSubmit} className="flex flex-col gap-3">
+    <input onChange={(e) => setEmail(e.target.value)} 
+    className="w-[400px] border border-gray-200 font-baskerville" 
+    type="text" 
+    placeholder="Email" 
+    />
+    <input onChange={(e) => setPassword(e.target.value)} 
+    className="w-[400px] border border-gray-200 font-baskerville" 
+    type="password" 
+    placeholder="Password" 
+    />
     <div className="flex items-center justify-end">
 <button className="bg-custom-green text-white font-baskerville rounded-md px-6 py-2">
     Login
     </button>
     </div>
-
-<div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-    Error message
+{error && ( 
+    <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+    {error}
 </div>
+)}
+
 
 <Link className="text-sm mt-3 text-right" href={"/register"}>
     Don't have an account? <span className="underline">Register</span>
