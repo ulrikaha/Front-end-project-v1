929b79d1a6134b8256c3596dd0b421cd2e8deb3c
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2023-10-24 12:54:48 +0200

    user auth working

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,7 +5,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 
 
-const authOptions = {
+export const authOptions = {
     providers: [
         CredentialsProvider({
             name: "credentials",
