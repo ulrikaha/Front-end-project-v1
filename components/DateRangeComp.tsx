8dcffd7b632e162a2c0c2c562cf279a6fd91b755
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2023-10-30 13:04:58 +0100

    bugg fixed with calendar

--- components/DateRangeComp.tsx
+++ components/DateRangeComp.tsx
@@ -1,100 +1,97 @@
-'use client';
+import { useEffect, useRef, useState } from 'react';
+import { DateRange } from 'react-date-range';
+import format from 'date-fns/format';
+import { useSearchContext } from '../context/SearchContext';
 
-
-import { useEffect, useRef, useState } from 'react'
-import { DateRange } from 'react-date-range'
-import format from 'date-fns/format'
-import { addDays } from 'date-fns'
-import { useSearchContext } from '../context/SearchContext'
-
-import 'react-date-range/dist/styles.css'
-import 'react-date-range/dist/theme/default.css'
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 const DateRangeComp = () => {
-  const { startDate, endDate, updateSearchCriteria } = useSearchContext();
+  const { startDate, endDate, selectedPackage, updateSearchCriteria } = useSearchContext();
 
-  // date state
+  // Initialize the range state with values from the context
   const [range, setRange] = useState([
     {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
-      key: 'selection'
-    }
-  ])
+      startDate,
+      endDate,
+      key: 'selection',
+    },
+  ]);
 
   // open close
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-  // get the target element to toggle 
-const refOne = useRef<HTMLDivElement>(null)
+  // get the target element to toggle
+  const refOne = useRef<HTMLDivElement>(null);
 
-useEffect(() => {
+  useEffect(() => {
     // event listeners
-    document.addEventListener("keydown", hideOnEscape, true)
-    document.addEventListener("click", hideOnClickOutside, true)
-}, [])
+    document.addEventListener('keydown', hideOnEscape, true);
+    document.addEventListener('click', hideOnClickOutside, true);
+  }, []);
 
-// hide dropdown on ESC press
-const hideOnEscape = (e: KeyboardEvent) => {
-    if( e.key === "Escape" ) {
-            setOpen(false)
+  // hide dropdown on ESC press
+  const hideOnEscape = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      setOpen(false);
     }
-}
+  };
 
-// Hide on outside click
-const hideOnClickOutside = (e: MouseEvent) => {
-    if( refOne.current && !(refOne.current.contains(e.target as Node)) ) {
-            setOpen(false)
+  // Hide on outside click
+  const hideOnClickOutside = (e: MouseEvent) => {
+    if (refOne.current && !(refOne.current.contains(e.target as Node))) {
+      setOpen(false);
     }
-}
+  };
 
   return (
     <div className="calendarWrap">
       <div className="ml-4 font-custom text-xl">Check in & Check out</div>
-     
- <div className="relative">
+
+      <div className="relative">
         <input
-        value={`${format(range[0].startDate, "dd/MM/yyyy")} to ${format(range[0].endDate, "dd/MM/yyyy")}`}
-        readOnly
-                className="inputBox border rounded-xl p-2 h-20 w-100  font-custom text-xl text-center"
-                onClick={ () => setOpen(open => !open) }
-              />
-             
+          value={`${format(range[0].startDate, 'dd/MM/yyyy')} to ${format(
+            range[0].endDate,
+            'dd/MM/yyyy'
+          )}`}
+          readOnly
+          className="inputBox border rounded-xl p-2 h-20 w-100 font-custom text-xl text-center"
+          onClick={() => setOpen((open) => !open)}
+        />
 
-              
-                {open && ( 
-                  <div ref={refOne} className="absolute bg-gray-200 border rounded p-2 z-10">
-                  <DateRange
-                    onChange={(item) => {
-                      if (item.selection.startDate && item.selection.endDate) {
-                        setRange([
-                          {
-                          startDate: item.selection.startDate,
-                          endDate: item.selection.endDate,
-                          key: 'selection'
-                        },
-                      ]);
-                      updateSearchCriteria(
-                        item.selection.startDate,
-                        item.selection.endDate,
-                        'all'
-                      );
-                      }
-                    }}
-                    editableDateInputs={true}
-                    moveRangeOnFirstSelection={false}
-                    ranges={range}
-                    months={1}
-                    direction="horizontal"
-                    className="calendarElement"
-                  />
-                </div>
-            )}
-            </div>
-            </div>
-          )
-        }
+        {open && (
+          <div ref={refOne} className="absolute bg-gray-200 border rounded p-2 z-10">
+            <DateRange
+              onChange={(item) => {
+                if (item.selection.startDate && item.selection.endDate) {
+                  setRange([
+                    {
+                      startDate: item.selection.startDate,
+                      endDate: item.selection.endDate,
+                      key: 'selection',
+                    },
+                  ]);
 
- 
+                  // Update the context state here with both date range and selected package
+                  updateSearchCriteria(
+                    item.selection.startDate,
+                    item.selection.endDate,
+                    selectedPackage
+                  );
+                }
+              }}
+              editableDateInputs={true}
+              moveRangeOnFirstSelection={false}
+              ranges={range}
+              months={1}
+              direction="horizontal"
+              className="calendarElement"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default DateRangeComp
+export default DateRangeComp;
